7eff05dd1815c433bee494fa65630a04859e43d4
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-06 19:30:51 +0000

    Install react-router-dom, add routing to App and index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Container>
+        <Routes>
+          <Route exact path="/" element={<h1>Home</h1>} />
+          <Route exact path="/sign-in" element={<h1>Sign in</h1>} />
+          <Route exact path="/register" element={<h1>Register</h1>} />
+        </Routes>
+      </Container>
     </div>
   );
 }
