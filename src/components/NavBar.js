4167b6493e4e7a483be28c5a01de19b5eb72ef13
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-06 18:16:38 +0000

    Add navbar using react-bootstrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,43 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const NavBar = () => {
-  return <div>NavBar goes here</div>;
+  return (
+    <Navbar expand="md" sticky="top" collapseOnSelect>
+      <Container fluid>
+        <Navbar.Brand href="#">Snaps</Navbar.Brand>
+        <Navbar.Toggle aria-controls="offcanvasNavbar" />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel"
+          placement="end"
+        >
+          <Offcanvas.Header closeButton>
+            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="justify-content-end flex-grow-1 pe-3">
+              <Nav.Link href="#action1">Home</Nav.Link>
+              <Nav.Link href="#action2">Sign in</Nav.Link>
+              <Nav.Link href="#action2">Register</Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+            </Form>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavBar;
