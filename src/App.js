9c6d854faba2502b01a6a97a6e8b0b5abb205e09
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-09 05:45:49 +0000

    Add context provider to reflect current user

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
@@ -6,19 +8,41 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("/dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
   return (
-    <div className={styles.App}>
-      <NavBar />
-      <Container>
-        <Routes>
-          <Route exact path="/" element={<h1>Home</h1>} />
-          <Route exact path="/sign-in" element={<SignInForm />} />
-          <Route exact path="/register" element={<SignUpForm />} />
-          <Route path="*" element={<p>Page not found</p>} />
-        </Routes>
-      </Container>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        <div className={styles.App}>
+          <NavBar />
+          <Container>
+            <Routes>
+              <Route exact path="/" element={<h1>Home</h1>} />
+              <Route exact path="/sign-in" element={<SignInForm />} />
+              <Route exact path="/register" element={<SignUpForm />} />
+              <Route path="*" element={<p>Page not found</p>} />
+            </Routes>
+          </Container>
+        </div>
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
   );
 }
 
