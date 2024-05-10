8ff956ca8c9e911f2a80da502340224d2675ecab
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-10 14:54:17 +0000

    Add comment to ImageAsset component

--- src/components/ImageAsset.js
+++ src/components/ImageAsset.js
@@ -1,8 +1,11 @@
 import React from "react";
 import styles from "../styles/ImageAsset.module.css";
 
+// Component to display image asset, src is the URL of the image
 const ImageAsset = ({ src }) => {
-  return <div className={styles.container}>{src && <img src={src} alt="" />}</div>;
+  return (
+    <div className={styles.container}>{src && <img src={src} alt="" />}</div>
+  );
 };
 
 export default ImageAsset;
