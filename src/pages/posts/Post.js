c52d02cd4ee60b5e1c3fc57d606a9338699f2447
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-11 21:36:26 +0000

    Add functions to like and unlike a post

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -6,6 +6,7 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Tooltip from "react-bootstrap/Tooltip";
 import { Link } from "react-router-dom";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
 import ImageAsset from "../../components/ImageAsset";
 
 const Post = (props) => {
@@ -24,10 +25,51 @@ const Post = (props) => {
     tags,
     updated_at,
     postPage,
+    setPosts,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+
+  // Function to handle liking a post and updating the posts state
+  const handleLike = async () => {
+    try {
+      const { data } = await axiosRes.post("/likes/", { post: id });
+
+      // Update posts state with new like
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? // Checks if the current post is the one being liked
+              { ...post, likes_count: post.likes_count + 1, like_id: data.id }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Function to handle un-liking a post and updating the posts state
+  const handleUnlike = async () => {
+    try {
+      await axiosRes.delete(`/likes/${like_id}/`);
+
+      // Update posts state by removing a like
+      setPosts((prevPosts) => ({
+        ...prevPosts,
+        results: prevPosts.results.map((post) => {
+          return post.id === id
+            ? // Checks if the current post is the one being un-liked
+              { ...post, likes_count: post.likes_count - 1, like_id: null }
+            : post;
+        }),
+      }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   // Set like button content based on user state
   let likeButtonContent = null;
@@ -44,13 +86,13 @@ const Post = (props) => {
     );
   } else if (!like_id && currentUser) {
     likeButtonContent = (
-      <span className="d-flex justify-content-end mt-1" onClick={() => {}}>
+      <span className="d-flex justify-content-end mt-1" onClick={handleLike}>
         <i className="fa-regular fa-thumbs-up"></i> {likes_count}
       </span>
     );
   } else if (like_id && currentUser) {
     likeButtonContent = (
-      <span className="d-flex justify-content-end mt-1" onClick={() => {}}>
+      <span className="d-flex justify-content-end mt-1" onClick={handleUnlike}>
         <i className="fa-solid fa-thumbs-up"></i> {likes_count}
       </span>
     );
