0e5b1641194d5e34b12a405020f95d4b4f8ccb2a
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-19 11:46:26 +0000

    Add modal to confirm album deletion

--- src/pages/albums/AlbumCard.js
+++ src/pages/albums/AlbumCard.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { OwnerDropdown } from "../../components/OwnerDropdown";
 import { axiosRes } from "../../api/axiosDefaults";
 import styles from "../../styles/AlbumCard.module.css";
 
 function AlbumCard({ album, id, onDelete, onEdit }) {
+  // State to control delete confirmation modal
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = () => setShowModal(false);
+  const handleShowModal = () => setShowModal(true);
+
   // Function to handle deleting an album
   const handleDelete = async () => {
-    try {
-      await axiosRes.delete(`/albums/${id}/`);
-      onDelete(id);
-    } catch (err) {
-      console.log(err);
+    if (showModal) {
+      try {
+        await axiosRes.delete(`/albums/${id}/`);
+        onDelete(id);
+      } catch (err) {
+        console.log(err);
+      }
     }
+    handleCloseModal();
   };
 
   // Function to handle editing an album
@@ -30,10 +40,29 @@ function AlbumCard({ album, id, onDelete, onEdit }) {
           </Link>
           <div className="flex-grow-1"></div>
           {/* Dropdown menu for album options */}
-          <OwnerDropdown handleDelete={handleDelete} handleEdit={handleEdit} />
+          <OwnerDropdown
+            handleDelete={handleShowModal}
+            handleEdit={handleEdit}
+          />
         </span>
         <Card.Title className={`${styles.AlbumFont}`}>{album.title}</Card.Title>
       </Card.Body>
+
+      {/* Modal for Delete Confirmation */}
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete Album</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this Album?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Card>
   );
 }
