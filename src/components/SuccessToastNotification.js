7175932ee4f5a2ab1d229a34b7845e69400b3d12
Markus Högberg <m.o.hogberg@gmail.com>
2024-05-25 09:34:43 +0000

    refactor: add fixed position and text color

--- src/components/SuccessToastNotification.js
+++ src/components/SuccessToastNotification.js
@@ -4,14 +4,18 @@ import ToastContainer from "react-bootstrap/ToastContainer";
 // Success Toast Notification
 function SuccessToastNotification({ show, onClose, position, message }) {
   return (
-    <ToastContainer className="p-3" position={position} style={{ zIndex: 1 }}>
+    <ToastContainer
+      className="p-3"
+      position={position}
+      style={{ zIndex: 1, position: "fixed", bottom: 0, right: 0 }}
+    >
       <Toast onClose={onClose} show={show} delay={4000} autohide bg="success">
         <Toast.Header>
           <img src="holder.js/20x20?text=%20" className="rounded me-2" alt="" />
           <strong className="me-auto">Success</strong>
           <small className="text-muted">just now</small>
         </Toast.Header>
-        <Toast.Body>{message}</Toast.Body>
+        <Toast.Body style={{ color: "white" }}>{message}</Toast.Body>
       </Toast>
     </ToastContainer>
   );
